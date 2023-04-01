20e8cacc67778ac803215d82db6cef57685bca12
ingalexvarela <ing.alexvarela@gmail.com>
2023-03-31 20:58:25 -0600

    Se prepara entorno para desarrollo de un ejercicio extra
    Signed-off-by: ingalexvarela <ing.alexvarela@gmail.com>

---
+++ EjerciciosExtra/Ejercicios en C++/TiendaPElectricos_clase en C++/Manejotienda.cpp
@@ -0,0 +1,24 @@
+/*
+Se desarrolla un programa que maneje una tienda de productos electrónicos que cuenta con tres tipos de productos:
+- Teléfonos móviles
+- Tabletas
+- Computadoras
+
+El programa permitirá al usuario ingresar nuevos productos, actualizar información de los existentes, eliminar productos y realizar búsquedas por marca, modelo, tipo de producto, entre otros criterios. Además, se podrá generar un reporte con la información de los productos disponibles en la tienda.
+*/
+
+#include <iostream> // Para entrada y salida por consola
+#include <string>   // Para manipular cadenas de texto
+#include <vector>   // Para manejar arreglos dinámicos
+#include <cmath>    // Para funciones matemáticas
+#include <fstream>  // Para leer y escribir archivos
+
+using namespace std;
+
+int main()
+{
+    cout << "Aquí vamos a desarrollar el ejemplo de clases" << endl;
+
+    // Código del programa
+    return 0;
+}
