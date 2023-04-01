0c993468a3bf4c53ac8fc5b9cf8b0e29d6f5aa30
ingalexvarela <ing.alexvarela@gmail.com>
2023-04-01 00:58:49 -0600

    Avance en ejercicio de uso de clases
    Signed-off-by: ingalexvarela <ing.alexvarela@gmail.com>

--- EjerciciosExtra/Ejercicios en C++/TiendaPElectricos_clase en C++/Manejotienda.cpp
+++ EjerciciosExtra/Ejercicios en C++/TiendaPElectricos_clase en C++/Manejotienda.cpp
@@ -26,33 +26,79 @@ public:
     void mostrarProducto()
     {
         // mostrar datos del producto
+        cout << "Marca: " << marca << endl;
+        cout << "Modelo: " << modelo << endl;
+        cout << "Número de serie: " << num_serie << endl;
+        cout << "Precio: $" << precio << endl;
     }
 };
 
 class TelefonoMovil : public Producto
 {
 public:
+    float pantalla_TELE;
+    string procesador_TELE;
+    int RAM_TELE;
+    int Almacenamiento_TELE;
+    float camara_TELE;
+
     void mostrarcelular()
     {
-        // mostrar datos especificos del producto
+        // Mostrar datos del producto base usando el método de la clase padre
+        mostrarProducto();
+        cout << "--------------------------------------" << endl;
+        // mostrar datos especificos del celular
+        cout << "pantalla: " << pantalla_TELE << " pulgadas" << endl;
+        cout << "procesador: " << procesador_TELE << endl;
+        cout << "RAM: " << RAM_TELE << " GB" << endl;
+        cout << "Almacenamiento: " << Almacenamiento_TELE << " GB" << endl;
+        cout << "Camara: " << camara_TELE << " MP" << endl;
     }
 };
 
 class Tableta : public Producto
 {
 public:
+    float pantalla_TB;
+    string procesador_TB;
+    int RAM_TB;
+    int Almacenamiento_TB;
+    float camara_TB;
+
     void mostrarTableta()
     {
-        // mostrar datos especificos del producto
+        // Mostrar datos del producto base usando el método de la clase padre
+        mostrarProducto();
+        cout << "--------------------------------------" << endl;
+        // Mostrar datos específicos de la tableta
+        cout << "Pantalla: " << pantalla_TB << " pulgadas" << endl;
+        cout << "Procesador: " << procesador_TB << endl;
+        cout << "RAM: " << RAM_TB << " GB" << endl;
+        cout << "Almacenamiento: " << Almacenamiento_TB << " GB" << endl;
+        cout << "Cámara: " << camara_TB << " MP" << endl;
     }
 };
 
 class ComputadoraPortatil : public Producto
 {
 public:
+    float pantalla_LP;
+    string procesador_LP;
+    int RAM_LP;
+    int Almacenamiento_LP;
+    float Grafica_LP;
+
     void mostrarlaptop()
     {
-        // mostrar datos especificos del producto
+        // Mostrar datos del producto base usando el método de la clase padre
+        mostrarProducto();
+        cout << "--------------------------------------" << endl;
+        // Mostrar datos específicos de la computadora portátil
+        cout << "Pantalla: " << pantalla_LP << " pulgadas" << endl;
+        cout << "Procesador: " << procesador_LP << endl;
+        cout << "RAM: " << RAM_LP << " GB" << endl;
+        cout << "Almacenamiento: " << Almacenamiento_LP << " GB" << endl;
+        cout << "Tarjeta gráfica: " << Grafica_LP << endl;
     }
 };
 
