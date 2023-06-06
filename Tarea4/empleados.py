289a53d9a5be319fc270fbdcaa05f02f07b194bc
ingalexvarela <ing.alexvarela@gmail.com>
2023-06-05 19:14:17 -0600

    Dando formato a la función imprimir_resultados
    Signed-off-by: ingalexvarela <ing.alexvarela@gmail.com>

--- Tarea4/empleados.py
+++ Tarea4/empleados.py
@@ -72,17 +72,36 @@ def buscar_por_salario(empleados, salario_minimo, salario_maximo):
 
 
 def imprimir_resultados(resultados):
-    if not resultados:
-        print("No se encontraron empleados que cumplan"
-              "con los criterios de búsqueda.")
-        return
-    for empleado in resultados:
-        print("Nombre:", empleado.nombre)
-        print("Correo:", empleado.correo)
-        print("Edad:", empleado.edad)
-        print("Departamento:", empleado.departamento)
-        print("Salario:", empleado.salario)
-        print()
+    if len(resultados) == 0:
+        # Imprime un mensaje indicando que no se encontraron resultados
+        print("No se encontraron resultados")
+    else:
+        # Encontrar el ancho máximo del campo "Departamento"
+        max_ancho_departamento = 0
+        for empleado in resultados:
+            max_ancho_departamento = max(
+                max_ancho_departamento, len(empleado.departamento))
+
+        # Imprime encabezados de la tabla
+        print("\tNombre y apellido\tCorreo electrónico"
+              "\t\tEdad\tDepartamento\t\t\tSalario")
+
+        # Imprime línea separadora
+        print("\t¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+              "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+              "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
+
+        for empleado in resultados:
+            print("\t{0}\t\t{1}\t\t{2}\t{3:<{4}}\t\t\t{5}"
+                  .format(empleado.nombre, empleado.correo,
+                          empleado.edad, empleado.departamento,
+                          max_ancho_departamento, empleado.salario))
+            # Imprime información en cada fila con alineación del salario
+
+        # Imprime línea separadora
+        print("\t¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+              "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+              "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
 
 
 def validar_entrada(entrada, patron):
