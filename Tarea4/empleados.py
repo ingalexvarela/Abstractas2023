f2ef1fa30318d3580ad69cdbd8e12449af32f551
ingalexvarela <ing.alexvarela@gmail.com>
2023-06-05 18:07:38 -0600

    cambio a función leer_archivo, para depurar información
    Signed-off-by: ingalexvarela <ing.alexvarela@gmail.com>

--- Tarea4/empleados.py
+++ Tarea4/empleados.py
@@ -19,11 +19,38 @@ def leer_archivo(ruta_archivo):
             next(reader)  # Saltar la primera fila (encabezados)
             for row in reader:
                 nombre, correo, edad, departamento, salario = row
+
+                # Validar campos
+                if not nombre or not isinstance(nombre, str):
+                    continue  # Saltar fila incompleta o incorrecta
+
+                if not correo or not isinstance(correo, str) or \
+                        "@" not in correo or not correo.endswith(".com"):
+                    continue  # Saltar fila incompleta o incorrecta
+
+                try:
+                    edad = int(edad)
+                    if not (0 <= edad <= 120):
+                        continue  # Saltar fila incompleta o incorrecta
+                except ValueError:
+                    continue  # Saltar fila incompleta o incorrecta
+
+                if not departamento or not isinstance(departamento, str):
+                    continue  # Saltar fila incompleta o incorrecta
+
+                try:
+                    salario = float(salario)
+                except ValueError:
+                    continue  # Saltar fila incompleta o incorrecta
+
+                # Crear objeto Empleado y agregar a la lista
                 empleado = Empleado(nombre, correo, edad,
                                     departamento, salario)
                 empleados.append(empleado)
+
     except FileNotFoundError:
         print("Error: No se encontró el archivo", ruta_archivo)
+
     return empleados
 
 
